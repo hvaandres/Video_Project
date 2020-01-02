23b897e9ce03170d4cf9ad076178b75a38ead27d
Andres Haro <hvaandres@icloud.com>
2020-01-02 13:38:51 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,43 +1,40 @@
+
 import csv
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 import time
 
-# Script to open multiple links one by one through a CSV File!
+
+
+
 
 def url_links(url_obj):
     reader = csv.DictReader(url_obj,delimiter = ',')
-    for line in reader:
-        url = line["url"]
-        tittle = line["tittle"]
-        print(url + tittle)
-        chromedriver = '/Users/hvaandres/Desktop/Dev_Projects/QA_Testing/Project_Video/chromedriver'
-        driver = webdriver.Chrome(chromedriver)
-        driver.get(url)
-        time.sleep(10)
-        #assert tittle in driver.title
-        driver.quit()
+    try: 
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--window-size=1920x1080")
+        chrome_options.add_argument("--incognito")
+        
+        for line in reader:
+            url = line["url"]
+            tittle = line["tittle"]
+            print(url + tittle)
+            chromedriver = '/usr/local/bin/chromedriver' 
+            driver = webdriver.Chrome(chromedriver, options=chrome_options) 
+            driver.get(url)
+            time.sleep(10)
+            driver.quit()
+#assert tittle in driver.title
+
+    except:
+
+        print("everything ready")
+
+    finally:
+
+        print("everything ready")
+
 if __name__ == "__main__":
-    with open ('/Users/hvaandres/Desktop/Dev_Projects/QA_Testing/Project_Video/URL.csv') as url_obj:
+    with open ('/home/oem/Desktop/Video_Project-master/URL.csv') as url_obj:
         url_links(url_obj)
-
-# Script to open multiple links one by one with a time.sleep of 200 seconds
-
-def url_links_200_seconds(url_obj):
-    reader = csv.DictReader(url_obj,delimiter = ',')
-    for line in reader:
-        url = line["url"]
-        tittle = line["tittle"]
-        print(url + tittle)
-        chromedriver = '/Users/hvaandres/Desktop/Dev_Projects/QA_Testing/Project_Video/chromedriver'
-        driver = webdriver.Chrome(chromedriver)
-        driver.get(url)
-        time.sleep(200)
-        #assert tittle in driver.title
-        driver.quit()
-if __name__ == "__main__":
-    with open ('/Users/hvaandres/Desktop/Dev_Projects/QA_Testing/Project_Video/URL.csv') as url_obj:
-        url_links_200_seconds(url_obj)
-
-# Script to open multiple links one by one with a time.sleep of 200 seconds
-
-   
